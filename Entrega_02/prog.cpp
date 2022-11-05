471aaf2da4cc5b031144ce4412658a6218046158
Arthur <80659617+artfrc@users.noreply.github.com>
2022-11-05 17:01:58 -0300

    Add files via upload

--- Entrega_02/prog.cpp
+++ Entrega_02/prog.cpp
@@ -2,6 +2,7 @@
 #include <fstream> // biblioteca para arquivos
 #include <cstdlib> // biblioteca para rand
 #include <vector>
+#include <ctime>
 
 #define MAX_TEXT 46
 
@@ -9,11 +10,11 @@ using namespace std;
 
 struct Register{
 
-	int nseq;
+	long long int nseq;
 	string text; // foi colocado uma restrição para que essa string nao ultrapasse o tamanho 46 (MAX_TEXT)
 
 	Register(){}
-	Register(int nseq, string text):nseq(nseq), text(text){};
+	Register(long long int nseq, string text):nseq(nseq), text(text){};
 
 };
 
@@ -27,10 +28,14 @@ string random_string();
 void read_random(fstream &f, int nseq);
 void isrt_at_end(fstream &f);
 void update_random(fstream &f, int nseq, string text);
+void delete_random(fstream &f, int nseq);
+void sequential_reading(fstream &f, int n);
+void random_access(fstream &f, int n);
 
 int main(void){
 
-	int op = -1, n_register;
+	int op = -1;
+	long long int n_register;
 	bool created_register = false;
 	string text;
 
@@ -40,12 +45,15 @@ int main(void){
 		cout << "[2]. Read random\n";
 		cout << "[3]. Insert end\n";
 		cout << "[4]. Update random\n";
+		cout << "[5]. Delete random\n";
+		cout << "[6]. Sequential reading\n";
+		cout << "[7]. Random access\n";
 		cout << "[0]. Exit\n";
 
 		cout << ">> Choose an option: ";
 		cin >> op;
 
-		if(op >= 0 && op <= 4 && op != 1 && op != 0 && !created_register){
+		if(op >= 0 && op <= 5 && op != 1 && op != 0 && !created_register){
 			cout << ">> Create heap file first\n\n";
 		}else{
 
@@ -67,7 +75,7 @@ int main(void){
 				case 2:
 					cout << ">> Enter registration number: ";
 					cin >> n_register;
-					read_random(File,n_register);					
+					read_random(File,n_register-1);					
 					
 					break;
 
@@ -80,8 +88,29 @@ int main(void){
 					cout << ">> Enter registration number: ";
 					cin >> n_register;
 					text = random_string();
-					update_random(File,n_register,text);
+					update_random(File,n_register-1,text);
 					
+					break;
+
+				case 5:
+					cout << ">> Enter registration number: ";
+					cin >> n_register;
+					delete_random(File,n_register-1);
+					
+					break;
+
+				case 6:
+					cout << ">> Enter registration number: ";
+					cin >> n_register;
+					sequential_reading(File,n_register);
+
+					break;
+
+				case 7:
+					cout << ">> Enter registration number: ";
+					cin >> n_register;
+					random_access(File,n_register);
+
 					break;
 
 				case 0:
@@ -99,33 +128,14 @@ int main(void){
 
 	}
 
-
-
-	//File.open("file.dat", ios::app); // ios::app posiciona o cursor no final do arquivo e insere o conteúdo sem substituir oq já possui lá
-
-/*
-	File.open("file.dat", ios::in|ios::ate);
-	if(File.is_open()){
-		cout << "Arquivo abriu\n";
-		
-		cout << File.tellg() << endl;
-
-		File.close();
-	}
-*/
 }
 
 
 void create_heap_file(int n_reg){
 
-	/*Register * r = (Register *) malloc(n_reg * sizeof(Register));
-	if(r == NULL){
-		return NULL;
-	}*/
-
 	// Criando e abrindo um arquivo para escrever nele (ios::out)
 	// Para adicionar texto ao arquivo, devemos usar ios::out | ios::app
-	File.open("file.dat", ios::out);
+	File.open("file.dat", ios::out|ios::binary);
 
 	// Escrevendo no arquivo
 	for(int i = 0; i < n_reg; ++i){
@@ -157,6 +167,11 @@ string random_string(){
 }
 
 void read_random(fstream &f, int nseq){
+
+	if(r.size() < nseq){
+		cout << ">> Record does not exist\n\n";
+		return;
+	}
 
 	// Abrindo meu arquivo no modo de entrada, ou seja, para ler ele (ios::in)
 	f.open("file.dat", ios::in);
@@ -201,16 +216,108 @@ void isrt_at_end(fstream &f){
 
 void update_random(fstream &f, int nseq, string text){
 
-	f.open("file.dat", ios::app|ios::binary);
+	if(r.size() < nseq){
+		cout << ">> Record does not exist\n\n";
+		return;
+	}
+
+	f.open("file.dat", ios::out);
 	if(f.is_open()){
-		//f.seekg((sizeof(char)*(MAX_TEXT + 2))*nseq, ios::beg);
-		f.seekp((sizeof(char)*(MAX_TEXT + 2))*nseq);
-		f.write(text.data(),text.size());
+
+		for(int i = 0; i < r.size(); i++){
+			if(r[i].nseq == nseq){
+				r[i].text = text;
+			}
+			f << r[i].text << endl;
+		}
 		f.close();
 		cout << ">> Updated successfully\n\n";
 	}else{
 		cout << ">> Failed to open file for reading" << endl;
 		return;
 	}
+
+}
+
+void delete_random(fstream &f, int nseq){
+
+	if(r.size() < nseq){
+		cout << ">> Record does not exist\n\n";
+		return;
+	}
+
+	r[nseq].nseq = abs(r[nseq].nseq) * (-1); 
+
+	cout << ">> Deleted successfully\n\n";
+
+}
+
+void sequential_reading(fstream &f, int n){
+
+
+	int valid = 0;
+	int pages = 0;
+	int cont = 0;
+	clock_t t;
+	t = clock();
+	for(int i = 0; i < r.size(); i++){
+
+		if(r[i].nseq >= 0){
+			valid++;
+		}
+		cont++;
+		if(cont == n){
+			pages++;
+			cont = 0;
+		}
+
+	}
+	t = clock() - t;
+	cout << ">> " << valid << " valid records\n";
+	cout << ">> " << pages << " pages\n";
+	cout << ">> " << ((float)t) / CLOCKS_PER_SEC << " seconds\n\n"; 
+
+}
+
+void random_access(fstream &f, int n){
+
+
+	clock_t t;
+	t = clock();
+	long long int valid = 0;
+	long long int not_valid = 0;
+	f.open("file.dat", ios::in);
+	if(f.is_open()){
+
+		while(n--){
+
+			long long int key = rand() % r.size() - 1;
+			if(r[key].nseq >= 0){
+
+				string line;
+
+				bool ok = false;
+
+				f.seekg((sizeof(char)*(MAX_TEXT + 2))*key, ios::beg);
+				char a[MAX_TEXT];
+				f.read(a, MAX_TEXT);
+
+				a[MAX_TEXT] = 0;
+				cout << a << endl;
+				valid++;
+			}else{
+				not_valid++;
+			}
+		}
+		f.close();
+	}else{
+		cout << ">> Failed to open file for reading" << endl;
+		return;
+	}
+	t = clock() - t;
+
+	cout << ">> Valid: " << valid << endl;
+	cout << ">> Not valid: " << not_valid << endl;
+	cout << ">> " << ((float)t) / CLOCKS_PER_SEC << " seconds\n\n"; 
 
 }
